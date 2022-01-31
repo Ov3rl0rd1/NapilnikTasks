6281d771f74880171c483b78a7bcd141e2914851
Overlord <nrbudanov1123@gmail.com>
2022-01-31 08:35:18 +0300

    Refactoring

--- Tasks/Weapon.cs
+++ Tasks/Weapon.cs
@@ -1,19 +1,36 @@
+using System;
 
+namespace Napilnik
+{
     class Weapon
     {
-        public int Damage;
-        public int Bullets;
+        public int Damage { get; private set; }
+        public int Bullets { get; private set; }
+
+        public Weapon(int damage, int bullets)
+        {
+            Damage = damage;
+            Bullets = bullets;
+        }
 
         public void Fire(Player player)
         {
-            player.Health -= Damage;
+            player.TryTakeDamage(Damage);
             Bullets -= 1;
         }
     }
 
     class Player
     {
-        public int Health;
+        private float _health;
+
+        public void TryTakeDamage(float damage)
+        {
+            if (damage > _health)
+                throw new ArgumentException(nameof(damage));
+
+            _health -= damage;
+        }
     }
 
     class Bot
@@ -25,3 +42,4 @@
             Weapon.Fire(player);
         }
     }
+}
