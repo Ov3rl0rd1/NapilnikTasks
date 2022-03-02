15e654e4d28779777d0f04a97212c2c222b59c65
Overlord <nrbudanov1123@gmail.com>
2022-03-02 08:10:42 +0300

    Refactoring

--- Tasks/OCP/OCP.cs
+++ Tasks/OCP/OCP.cs
@@ -2,7 +2,7 @@
 
 namespace IMJunior
 {
-    internal class Program
+    public class Program
     {
         public static void Main(string[] args)
         {
--- Tasks/OCP/PaymentHandler.cs
+++ Tasks/OCP/PaymentHandler.cs
@@ -5,7 +5,16 @@ namespace IMJunior
 {
     public class PaymentHandler
     {
-        public IReadOnlyCollection<IPayment> PaymentMethods = new IPayment[] { new QIWI(), new WebMoney(), new Card() };
+        public IReadOnlyCollection<IPayment> PaymentMethods;
+
+        public PaymentHandler()
+        {
+            List<IPayment> paymentMethods = new List<IPayment>();
+            paymentMethods.Add(CreatePaymentMethod("QIWI", "Перевод на страницу QIWI..."));
+            paymentMethods.Add(CreatePaymentMethod("WebMoney", "Вызов API WebMoney..."));
+            paymentMethods.Add(CreatePaymentMethod("Card", "Вызов API банка эмитера карты Card..."));
+            PaymentMethods = paymentMethods;
+        }
 
         public void ShowPaymentResult(string systemId)
         {
@@ -21,6 +30,11 @@ namespace IMJunior
             Console.WriteLine($"Вы оплатили с помощью {systemId}");
             Console.WriteLine($"Проверка платежа через {systemId}...");
             Console.WriteLine("Оплата прошла успешно!");
+        }
+
+        protected virtual IPayment CreatePaymentMethod(string id, string callAPI)
+        {
+            return new Payment(id, callAPI);
         }
     }
 }
