d7fc1f2650e1029dfec61a8eaae4faf95346921a
Overlord <nrbudanov1123@gmail.com>
2022-03-23 08:08:33 +0300

    Refactoring

---
+++ SQLTask/SQLTask/PassportFinder.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Data;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SQLTask
+{
+    static class PassportFinder
+    {
+        public static string Find(string rawData)
+        {
+            string passport = GetPassportSeries(rawData);
+            if (passport.Length < 10 || passport.Length == 0)
+                return "Неверный формат серии или номера паспорта";
+
+            DataTable dataTable = GetDataTable(passport);
+
+            if (dataTable.Rows.Count > 0)
+            {
+                if (Convert.ToBoolean(dataTable.Rows[0].ItemArray[1]))
+                    return "По паспорту «" + passport + "» доступ к бюллетеню на дистанционном электронном голосовании ПРЕДОСТАВЛЕН";
+                else
+                    return "По паспорту «" + passport + "» доступ к бюллетеню на дистанционном электронном голосовании НЕ ПРЕДОСТАВЛЯЛСЯ";
+            }
+            else
+                return "Паспорт «" + rawData + "» в списке участников дистанционного голосования НЕ НАЙДЕН";
+        }
+
+        private static DataTable GetDataTable(string passport)
+        {
+            if (SQLDB.IsConnected == false)
+                throw new InvalidOperationException();
+
+            string commandText = $"select * from passports where num='{ComputeSha256Hash(passport)}' limit 1;";
+            DataTable dataTable = SQLDB.GetDataTable(commandText);
+
+            SQLDB.Disconnect();
+
+            return dataTable;
+        }
+
+        private static string GetPassportSeries(string rawData)
+        {
+            return rawData.Trim().Replace(" ", string.Empty);
+        }
+
+        private static string ComputeSha256Hash(string value)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            using (SHA256 hash = SHA256.Create())
+            {
+                Encoding enc = Encoding.UTF8;
+                byte[] result = hash.ComputeHash(enc.GetBytes(value));
+
+                foreach (byte b in result)
+                    stringBuilder.Append(b.ToString("x2"));
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ SQLTask/SQLTask/SQLDB.cs
@@ -0,0 +1,55 @@
+﻿using Devart.Data.SQLite;
+using System;
+using System.Data;
+using System.IO;
+using System.Reflection;
+
+namespace SQLTask
+{
+    class SQLDB
+    {
+        public static bool IsConnected => _isConnected;
+
+        private static bool _isConnected;
+        private static SQLiteConnection _connection;
+
+        public static void Connect()
+        {
+            if (_isConnected)
+                return;
+
+            string connectionString = string.Format("Data Source=" + Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\db.sqlite");
+            try
+            {
+                _connection = new SQLiteConnection(connectionString);
+                _connection.Open();
+                _isConnected = true;
+            }
+            catch (SQLiteException ex)
+            {
+                if ((int)ex.ErrorCode != 1)
+                    return;
+                throw new Exception("Файл db.sqlite не найден. Положите файл в папку вместе с exe.");
+            }
+        }
+
+        public static DataTable GetDataTable(string command)
+        {
+            if (_isConnected == false)
+                throw new InvalidOperationException();
+
+            SQLiteDataAdapter sqLiteDataAdapter = new SQLiteDataAdapter(new SQLiteCommand(command, _connection));
+            DataTable dataTable1 = new DataTable();
+            DataTable dataTable2 = dataTable1;
+            sqLiteDataAdapter.Fill(dataTable2);
+            return dataTable1;
+        }
+
+        public static void Disconnect()
+        {
+            _isConnected = false;
+            _connection.Close();
+            _connection = null;
+        }
+    }
+}
